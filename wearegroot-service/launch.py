3c00d6465bfee690ceb54f5b8480fc45094e7f96
Mohanish_personal <mpgadge@gmail.com>
2023-07-13 20:14:05 +0530

    docker support added for backend service

---
+++ wearegroot-service/launch.py
@@ -0,0 +1,9 @@
+from flask import Flask
+
+helloworld = Flask(__name__)
+
+@helloworld.route("/")
+def run():
+    return "{\"message\":\"Hello World Python - v1\"}"
+if __name__ == "__main__":
+    helloworld.run(host="0.0.0.0", port=1988, debug=True)
