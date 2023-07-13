772edd60e5081a1c83712535e14ab1989961ad3c
Mohanish_personal <mpgadge@gmail.com>
2023-07-14 02:04:41 +0530

    docker support added for backend service

--- wearegroot-service/launch.py
+++ wearegroot-service/launch.py
@@ -1,27 +1,31 @@
 from flask import Flask, jsonify,request
+import os
 from vertexai.preview.language_models import TextGenerationModel
-helloworld = Flask(__name__)
+from PyPDF2 import PdfReader
 
-@helloworld.route("/")
-def run():
-    generation_model = TextGenerationModel.from_pretrained("text-bison@001")
+app = Flask(__name__)
 
-    prompt = """
-            Provide a very short summary, no more than three sentences, for the following article:
+generation_model = TextGenerationModel.from_pretrained("text-bison@001")
 
-            Our quantum computers work by manipulating qubits in an orchestrated fashion that we call quantum algorithms.
-            The challenge is that qubits are so sensitive that even stray light can cause calculation errors — and the problem worsens as quantum computers grow.
-            This has significant consequences, since the best quantum algorithms that we know for running useful applications require the error rates of our qubits to be far lower than we have today.
-            To bridge this gap, we will need quantum error correction.
-            Quantum error correction protects information by encoding it across multiple physical qubits to form a “logical qubit,” and is believed to be the only way to produce a large-scale quantum computer with error rates low enough for useful calculations.
-            Instead of computing on the individual qubits themselves, we will then compute on logical qubits. By encoding larger numbers of physical qubits on our quantum processor into one logical qubit, we hope to reduce the error rates to enable useful quantum algorithms.
+@app.route("/summarize", methods = ['POST'])
+def summarize(): 
+    if request.method == 'POST':  
+        f = request.files['file']
+        f.save(f.filename)        
+        
+        reader = PdfReader(f.filename)
+        pages = reader.pages
 
-            Summary:
+    summary = ''
+    for i in range(len(pages)):
+        text = pages[i].extract_text().strip()
+        print(f"{text} \n\n")
+        summary = summary + text
 
-            """
+    response = generation_model.predict(summary, temperature=0.2, max_output_tokens=1024, top_k=40, top_p=0.8).text
     
-    response = generation_model.predict(prompt, temperature=0.2, max_output_tokens=1024, top_k=40, top_p=0.8).text
-    return jsonify ({'response': response})
-    # return "{\"message\":\"response\"}"
+    os.remove(f.filename)
+    return jsonify ({'summary': response})
+    
 if __name__ == "__main__":
-    helloworld.run(host="0.0.0.0", port=1988, debug=True)
+    app.run(host="0.0.0.0", port=1988, debug=True)
