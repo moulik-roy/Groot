476d3045b8d62e71f5ae49d86cc005c195c814ad
phoenix <phoenixh577@gmail.com>
2023-07-14 04:44:16 +0530

    added classify logic

--- wearegroot-service/launch.py
+++ wearegroot-service/launch.py
@@ -43,8 +43,29 @@ def summarize():
                     """
     prompt = prompt_template.format(text=concatenated_text[:30000])
     summary = generation_model.predict(prompt=prompt, max_output_tokens=1024).text
+
+    full_stack = ["Angular" , "React" , "Java" , "springboot" , "CI" , "Jenkins" , "Teamcity" , "Git"]
+    Big_data = ["Spark" , "Scala" , "Python" , "Hive" , "NoSQL"]
+    Java_developer = ["Java8","Springboot","Hibernate", "JPA", "Oracle"]
+    Ui_developer = ["Angular","React","Javascript","Typescript","Bootstrap","HTML","CSS"]
+
+    java_developer_re = re.compile(r'\bJava\b | \bSpringboot\b | \bHibernate\b | \bjpa\b | \boracle\b' , flags=re.I | re.X)
+    big_data_re = re.compile(r'\bSpark\b | \bScala\b | \bPython\b | \bHive\b | \bNoSQL\b' , flags=re.I | re.X)
+    full_stack_re = re.compile(r'\bAngular\b | \bReact\b | \bJava\b | \bspringboot\b | \bCI\b | \bJenkins\b | \bTeamcity\b | \bGit\b' , flags=re.I | re.X)
+    ui_developer_re = re.compile(r'\bAngular\b | \bReact\b | \bJavascript\b | \bTypescript\b | \bBootstrap\b | \bHTML\b | \bCSS\b' , flags=re.I | re.X)
+
+    javaDevMatch = java_developer_re.findall(summary)
+    bigDataMatch = big_data_re.findall(summary)
+    fullStackMatch = full_stack_re.findall(summary)
+    uiDeveloperMatch = ui_developer_re.findall(summary)
+
+    java_developer_percentage = len(javaDevMatch)/len(full_stack)*100
+    big_data_percentage = len(bigDataMatch)/len(Big_data)*100
+    full_stack_percentage = len(fullStackMatch)/len(Java_developer)*100
+    ui_developer_percentage = len(uiDeveloperMatch)/len(Ui_developer)*100
+
     os.remove(f.filename)
-    return jsonify ({'summary': summary})
+    return jsonify ({'summary': summary ,"Java_Developer": java_developer_percentage,"Bigdata_Developer":big_data_percentage,"FullStack_Developer":full_stack_percentage,"UI_Developer":ui_developer_percentage})
     
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8080, debug=True)
